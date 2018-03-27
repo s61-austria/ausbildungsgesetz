6984635b93578065966293511cd8c09c6ad7bdf1
jandie <eidnaj@live.nl>
2018-03-27 13:45:28 +0200

    Regenerating invoices implemented in ui

--- src/actions/invoice.js
+++ src/actions/invoice.js
@@ -3,6 +3,9 @@ import request from 'superagent-es6-promise'
 export const INVOICES_FETCHING = "INVOICES_FETCHING";
 export const INVOICES_FETCHED = "INVOICES_FETCHED";
 
+export const INVOICE_GENERATING = "";
+export const INVOICE_REGENERATED = "INVOICE_REGENERATED";
+
 export function fetchInvoices() {
     return (dispatch) => {
         dispatch({type: INVOICES_FETCHING});
@@ -10,7 +13,23 @@ export function fetchInvoices() {
         request.get("http://localhost:8080/government/api/invoices")
             .then(result =>
                 dispatch({
-                    type: INVOICES_FETCHED, data: {invoices: result.body }
+                    type: INVOICES_FETCHED, data: {invoices: result.body}
                 }))
+    }
+}
+
+export function regenerateInvoice(invoice) {
+    return (dispatch) => {
+        dispatch({type: INVOICE_GENERATING, invoice});
+
+        request.get(`http://localhost:8080/government/api/invoices/regenerate/${invoice.uuid}`)
+            .then(result =>
+                dispatch({
+                    type: INVOICE_REGENERATED, data: result.body
+                }))
+            .catch(error => dispatch({
+                type: "INVOICE_GENERATION_FAILED",
+                error
+            }))
     }
 }
