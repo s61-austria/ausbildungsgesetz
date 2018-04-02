1d473cd9e75acae64061cf50070b0244ce6c12bd
guillaime <guillaimejanssen@msn.com>
2018-04-02 19:27:46 +0200

    Removed unused import and unnecessary string adding

--- src/components/vehicles/VehicleList.js
+++ src/components/vehicles/VehicleList.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {VehicleItem} from './VehicleItem'
-import {vehicle} from "../../reducers/vehicle";
 
 export class VehicleList extends React.Component {
     constructor(props) {
